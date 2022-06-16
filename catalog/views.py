5a98fb87c8376027b0393ec9ae84dc0a2fba109f
J053-3NR1QU3 <107213222+J053-3NR1QU3@users.noreply.github.com>
2022-06-16 01:31:54 -0500

    Add files via upload

---
+++ catalog/views.py
@@ -0,0 +1,127 @@
+from django.views import generic
+from django.shortcuts import render
+
+from django.contrib.auth.decorators import permission_required
+
+from .models import Book, Author, BookInstance, Genre
+
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+import datetime
+
+from .forms import RenewBookForm
+
+def index(request):
+    """
+    Función vista para la página inicio del sitio.
+    """
+    # Genera contadores de algunos de los objetos principales
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    # Libros disponibles (status = 'a')
+    num_instances_available = BookInstance.objects.filter(
+        status__exact='a').count()
+    # El 'all()' esta implícito por defecto.
+    num_authors = Author.objects.count()
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_visits': num_visits,
+    }
+
+    # Renderiza la plantilla HTML index.html con los datos en la variable contexto
+    return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    # your own name for the list as a template variable
+    context_object_name = 'my_book_list'
+    queryset = Book.objects.filter(title__icontains='war')[
+        :5]  # Get 5 books containing the title war
+    # Specify your own template name/location
+    template_name = 'books/my_arbitrary_template_name_list.html'
+    paginate_by = 10
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
+def book_detail_view(request, pk):
+    try:
+        book_id = Book.objects.get(pk=pk)
+    except Book.DoesNotExist:
+        raise Http404("Book does not exist")
+
+    #book_id=get_object_or_404(Book, pk=pk)
+
+    return render(
+        request,
+        'catalog/book_detail.html',
+        context={'book': book_id, }
+    )
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
+    """
+    Generic class-based view listing books on loan to current user.
+    """
+    model = BookInstance
+    template_name ='catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+
+@permission_required('catalog.can_mark_returned')
+def renew_book_librarian(request, pk):
+    """
+    View function for renewing a specific BookInstance by librarian
+    """
+    book_inst=get_object_or_404(BookInstance, pk = pk)
+
+    if request.method == 'POST':
+
+       
+        form = RenewBookForm(request.POST)
+
+        
+        if form.is_valid():
+           
+            book_inst.due_back = form.cleaned_data['renewal_date']
+            book_inst.save()
+
+           
+            return HttpResponseRedirect(reverse('all-borrowed') )
+
+    
+    else:
+        proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
+        form = RenewBookForm(initial={'renewal_date': proposed_renewal_date,})
+
+    return render(request, 'catalog/book_renew_librarian.html', {'form': form, 'bookinst':book_inst})
+
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Author
+
+class AuthorCreate(CreateView):
+    model = Author
+    fields = '__all__'
+    initial={'date_of_death':'05/01/2018',}
+
+class AuthorUpdate(UpdateView):
+    model = Author
+    fields = ['first_name','last_name','date_of_birth','date_of_death']
+
+class AuthorDelete(DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
